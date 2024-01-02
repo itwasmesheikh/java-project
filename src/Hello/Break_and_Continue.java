9399272e412677e6ce13ff0601d8111b4663ef63
itwasmesheikh <nomansheikh2664@gmail.com>
2024-01-02 22:23:53 +0600

    Break and Continue

--- src/Hello/Break_and_Continue.java
+++ src/Hello/Break_and_Continue.java
@@ -5,8 +5,11 @@ public class Break_and_Continue {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(i);
-			if (i == 5) {
+			/*if (i == 5) {
 				break;
+			}*/
+			if(i == 5) {
+				continue;
 			}
 		}
 
