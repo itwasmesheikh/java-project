190c3624d7702852c44452df993b46a2bfe5c49d
itwasmesheikh <nomansheikh2664@gmail.com>
2024-01-02 21:47:37 +0600

    first commit

---
+++ src/Boolean/Boolean.java
@@ -0,0 +1,13 @@
+package Boolean;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		boolean x;
+		x = (5>4)?true:false;
+		
+		System.out.println(x);
+		
+	}
+
+}
---
+++ src/Hello/For_Loop.java
@@ -0,0 +1,21 @@
+package Hello;
+
+public class For_Loop {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+
+			System.out.println(i);
+
+		}
+
+		for (int i = 1, j = 1; i <= 10 && j <= 10; i++, j++) {
+			i++;
+			j++;
+			System.out.println("i = " + i + " and j = " + j);
+		}
+
+	}
+
+}
