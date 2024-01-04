867a72ea7e055fd3aee626691d346da5394a1e56
itwasmesheikh <nomansheikh2664@gmail.com>
2024-01-04 18:39:06 +0600

    Classes

---
+++ src/Classes/Hello.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.setSpeed(120);
+		System.out.println(car.getSpeed());
+
+	}
+
+}
