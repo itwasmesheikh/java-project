7b96588cd238858e43b52e18248cd2af74bd2b40
itwasmesheikh <nomansheikh2664@gmail.com>
2024-02-05 23:30:47 +0600

    Anonymous

--- src/innerClass/ShopTwo.java
+++ src/innerClass/ShopTwo.java
@@ -3,11 +3,13 @@ package innerClass;
 public class ShopTwo {
 
 	public static void main(String[] args) {
-		DoorTwo door = new DoorTwo();
-		if(door.isLocked(args[0]))
+		//DoorTwo door = new DoorTwo();
+		if(new DoorTwo().isLocked(args[0]))
 			System.out.println("Shop has closed. Please visit later");
 		else
 			System.out.println("Welcome! The shop is open.");
+		System.out.println(args[1]);
+		System.out.println(args[2]);
 
 	}
 
